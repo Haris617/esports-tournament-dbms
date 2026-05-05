b45621886340e656f7860b37946a38c505286aa0
Haris Wasim <toughrider617@gmail.com>
2026-05-05 23:07:37 +0500

    Update run_all.sql

--- run_all.sql
+++ run_all.sql
@@ -12,16 +12,16 @@
     3. Execute this file.
 */
 
-:setvar ProjectRoot "C:\Path\To\esports-tournament-dbms"
+:setvar ProjectRoot "C:\Users\Haris\Desktop\Github\esports-tournament-dbms"
 
-:r "$(ProjectRoot)\sql\00_database_setup.sql"
-:r "$(ProjectRoot)\sql\01_schema_tables.sql"
-:r "$(ProjectRoot)\sql\02_test_tables_and_ddl_changes.sql"
-:r "$(ProjectRoot)\sql\03_seed_data.sql"
-:r "$(ProjectRoot)\sql\04_preview_tables.sql"
-:r "$(ProjectRoot)\sql\05_dml_updates_deletes.sql"
-:r "$(ProjectRoot)\sql\06_drl_select_queries.sql"
-:r "$(ProjectRoot)\sql\07_subqueries.sql"
-:r "$(ProjectRoot)\sql\08_aggregates_text_search.sql"
-:r "$(ProjectRoot)\sql\09_join_queries.sql"
-:r "$(ProjectRoot)\sql\10_function_queries.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\00_database_setup.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\01_schema_tables.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\02_test_tables_and_ddl_changes.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\03_seed_data.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\04_preview_tables.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\05_dml_updates_deletes.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\06_drl_select_queries.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\07_subqueries.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\08_aggregates_text_search.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\09_join_queries.sql"
+:r "C:\Users\Haris\Desktop\Github\esports-tournament-dbms\sql\10_function_queries.sql"
