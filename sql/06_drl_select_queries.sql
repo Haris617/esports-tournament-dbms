adc67332a6ff79c2df1803d7a29c717b1ea7b8b6
Haris Wasim <toughrider617@gmail.com>
2026-05-07 00:01:44 +0500

    Update tournament filter and game ordering
    Adjust Q28 to filter tournaments that contain 'Valorant' or 'Football' in the title (replacing the previous 'Cup'/'Challenge' filters). Also make Q30 explicitly order games ascending with ORDER BY game_name ASC to clarify sorting direction.

--- sql/06_drl_select_queries.sql
+++ sql/06_drl_select_queries.sql
@@ -35,11 +35,11 @@ FROM tbl_Players
 WHERE is_active_flag = 1
   AND username LIKE 'a%';
 
--- Q28. OR: Show tournaments having Cup or Challenge in title.
+-- Q28. OR: Show tournaments having Valorant or Football in title.
 SELECT *
 FROM tbl_Tournaments
-WHERE title LIKE '%Cup%'
-   OR title LIKE '%Challenge%';
+WHERE title LIKE '%Valorant%'
+   OR title LIKE '%Football%';
 
 -- Q29. GROUP BY: Count players in each team.
 SELECT team_id,
@@ -50,7 +50,7 @@ GROUP BY team_id;
 -- Q30. ORDER BY: Show games in alphabetical order.
 SELECT *
 FROM tbl_Games
-ORDER BY game_name;
+ORDER BY game_name ASC;
 
 -- Q31. HAVING: Show teams having 2 or more players.
 SELECT team_id, COUNT(player_id) AS total_players
